ead53832c451ce25bd642911ef55544f70e16dde
JR Behaeghel <jean.remy@gmail.com>
2017-07-11 10:03:50 -0400

    Delete scrap.R
    Clean up repository

--- scrap.R
+++
@@ -1,4 +0,0 @@
-m %>% addProviderTiles("NASAGIBS.ModisTerraTrueColorCR",
-                 options = providerTileOptions(time =Sys.Date()-1, opacity = 0.5)) %>%
-        addMarkers(lng=-71.0589, lat=42.3601, popup="The Hub: Boston MA")
-
